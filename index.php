3ba6b4890af66644c950f0de623503aa2f0dfaa7
Ronny Gutierrez <ronny.icne@gmail.com>
2018-05-12 22:17:16 -0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,113 @@
+<html>
+	<head>
+		<title>Online Shopping</title>
+		<link rel="stylesheet" href="css/bootstrap.css">
+		<link rel="stylesheet" href="css/style.css">
+		<script src="js/jquery.js"></script>
+		<script src="js/bootstrap.js"></script>
+	</head>
+	<body>
+		
+		<div class="container">
+			<div class="row">
+				<div class="col-md-3">
+					<div class="col-md-12  single-item noPadding">
+						<div class="top"><img src="images/products/product1.jpg"></div>
+						<div class="bottom">
+							<h3 class="item-title"><a href="product.php">Beautiful Watch</a></h3>
+							<div class="pull-right cutted-price text-muted"><del>$ 500/=</del></div>
+							<div class="clearfix"></div>
+							<div class="pull-right discounted-price">$ 450/=</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-3">
+					<div class="col-md-12  single-item noPadding">
+						<div class="top"><img src="images/products/product1.jpg"></div>
+						<div class="bottom">
+							<h3 class="item-title"><a href="product.php">Beautiful Watch</a></h3>
+							<div class="pull-right cutted-price text-muted"><del>$ 500/=</del></div>
+							<div class="clearfix"></div>
+							<div class="pull-right discounted-price">$ 450/=</div>
+						</div>
+					</div>
+				</div><div class="col-md-3">
+					<div class="col-md-12  single-item noPadding">
+						<div class="top"><img src="images/products/product1.jpg"></div>
+						<div class="bottom">
+							<h3 class="item-title"><a href="product.php">Beautiful Watch</a></h3>
+							<div class="pull-right cutted-price text-muted"><del>$ 500/=</del></div>
+							<div class="clearfix"></div>
+							<div class="pull-right discounted-price">$ 450/=</div>
+						</div>
+					</div>
+				</div><div class="col-md-3">
+					<div class="col-md-12  single-item noPadding">
+						<div class="top"><img src="images/products/picture3.jpg"></div>
+						<div class="bottom">
+							<h3 class="item-title"><a href="product.php">Beautiful Watch</a></h3>
+							<div class="pull-right cutted-price text-muted"><del>$ 500/=</del></div>
+							<div class="clearfix"></div>
+							<div class="pull-right discounted-price">$ 450/=</div>
+						</div>
+					</div>
+				</div><div class="col-md-3">
+					<div class="col-md-12  single-item noPadding">
+						<div class="top"><img src="images/products/picture2.jpg"></div>
+						<div class="bottom">
+							<h3 class="item-title"><a href="product.php">Beautiful Watch</a></h3>
+							<div class="pull-right cutted-price text-muted"><del>$ 500/=</del></div>
+							<div class="clearfix"></div>
+							<div class="pull-right discounted-price">$ 450/=</div>
+						</div>
+					</div>
+				</div><div class="col-md-3">
+					<div class="col-md-12  single-item noPadding">
+						<div class="top"><img src="images/products/picture1.jpg"></div>
+						<div class="bottom">
+							<h3 class="item-title"><a href="product.php">Beautiful Watch</a></h3>
+							<div class="pull-right cutted-price text-muted"><del>$ 500/=</del></div>
+							<div class="clearfix"></div>
+							<div class="pull-right discounted-price">$ 450/=</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-3">
+					<div class="col-md-12  single-item noPadding">
+						<div class="top"><img src="images/products/product1.jpg"></div>
+						<div class="bottom">
+							<h3 class="item-title"><a href="product.php">Beautiful Watch</a></h3>
+							<div class="pull-right cutted-price text-muted"><del>$ 500/=</del></div>
+							<div class="clearfix"></div>
+							<div class="pull-right discounted-price">$ 450/=</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-3">
+					<div class="col-md-12  single-item noPadding">
+						<div class="top"><img src="images/products/product1.jpg"></div>
+						<div class="bottom">
+							<h3 class="item-title"><a href="product.php">Beautiful Watch</a></h3>
+							<div class="pull-right cutted-price text-muted"><del>$ 500/=</del></div>
+							<div class="clearfix"></div>
+							<div class="pull-right discounted-price">$ 450/=</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-3">
+					<div class="col-md-12  single-item noPadding">
+						<div class="top"><img src="images/products/product1.jpg"></div>
+						<div class="bottom">
+							<h3 class="item-title"><a href="product.php">Beautiful Watch</a></h3>
+							<div class="pull-right cutted-price text-muted"><del>$ 500/=</del></div>
+							<div class="clearfix"></div>
+							<div class="pull-right discounted-price">$ 450/=</div>
+						</div>
+					</div>
+				</div>
+				
+			</div>
+		</div><div class="clearfix"></div>
+		
+	</body>
+</html>
