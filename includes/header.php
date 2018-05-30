ed663aebd33e89015755ad506523378e79f684e4
Ronny Gutierrez <ronny.icne@gmail.com>
2018-05-29 21:55:24 -0700

    9:55pm

--- includes/header.php
+++ includes/header.php
@@ -4,7 +4,7 @@
               <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
                 <span class="icon-bar"></span>  <span class="icon-bar"></span>  <span class="icon-bar"></span>
               </button>
-              <a class="navbar-brand" href="index.php">The Online Shop</a>
+              <a class="navbar-brand " href="index.php">Official</a>
              </div>
              <div class="collapse navbar-collapse" id="myNavbar">
             <ul class="nav navbar-nav navbar-left">
